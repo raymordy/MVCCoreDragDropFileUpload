72380c85d0ef8a8f9d3403c81c81326af028b918
Ray Mordy <ray@mordy.co.uk>
2019-03-07 22:05:57 +0000

    Add project files.

---
+++ MVCFileUploader/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCFileUploader.Models;
+
+namespace MVCFileUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _environment;
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach(var file in files)
+            {
+                if(file.Length > 0)
+                {
+                    string folderRoot = Path.Combine(_environment.ContentRootPath, "Uploads");
+                    string filePath =  Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    filePath = Path.Combine(folderRoot, filePath);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            return Ok(new { count = files.Count, size });
+        }
+    }
+}
